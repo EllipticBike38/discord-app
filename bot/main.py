5cb6c44f80a83f586368b5567e7c24d4dbbccaf2
EllipticBike38 <andrea.mazzini00@gmail.com>
2022-08-15 02:32:48 +0200

    personalized with start stop info and status tasks

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,47 @@
-import os
+
 from discord.ext import commands
+from python_aternos import Client
+import credentials
 
 bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = credentials.TOKEN
+aternos = Client.from_credentials(credentials.aternos_user, credentials.aternos_pass)
+myserver=aternos.get_server(credentials.server_id)
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}({bot.user.id})")
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+async def startServer(ctx):
+    try:
+        myserver.start()
+    except commands.errors.CommandInvokeError:
+        await ctx.send('already online')
+    await ctx.send('Starting in a few seconds...') 
+  
+@bot.command()
+async def stopServer(ctx):
+    myserver.stop()
+    await ctx.send('Stopping...') 
+    
+    
+    
+@bot.command()
+async def status(ctx):
+    await ctx.send(myserver.status)
+    
+@bot.command()
+async def infos(ctx):
+    info_string=f'''
+    Server address: {myserver.address}
+    
+    players: {myserver.players_count}/{myserver.slots}:
+    {",".join(myserver.players_list)}
+    
+    Minecraft {myserver.edition.name.capitalize()} Edition: {myserver.version}, {myserver.software}    
+    '''
+    await ctx.send(info_string)
 
 if __name__ == "__main__":
     bot.run(TOKEN)
