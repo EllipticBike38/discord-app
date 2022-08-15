bf553b17fe46b0e9114c16093dbd7144f49d87f5
EllipticBike38 <andrea.mazzini00@gmail.com>
2022-08-15 03:00:41 +0200

    environ

--- bot/main.py
+++ bot/main.py
@@ -4,9 +4,9 @@ from python_aternos import Client
 import os
 
 bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv('TOKEN')
-aternos = Client.from_credentials(os.getenv('aternos_user'), os.getenv('aternos_pass'))
-myserver=aternos.get_server(os.getenv('server_id'))
+TOKEN = os.environ['TOKEN']
+aternos = Client.from_credentials(os.environ['aternos_user'], os.environ['aternos_pass'])
+myserver=aternos.get_server(os.environ['server_id'])
 
 @bot.event
 async def on_ready():
